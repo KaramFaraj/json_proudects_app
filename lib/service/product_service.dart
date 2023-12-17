5ac45d4e2ff69867bec9d88999614e1a02069967
KaramFaraj <karam.faraj.ac@gmail.com>
2023-12-17 22:43:55 +0300

    make a service layer

---
+++ lib/service/product_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+
+abstract class BaseProductsService {
+  Dio dio = Dio();
+  String baseproducturl = 'https://dummyjson.com/products/';
+  late Response response;
+}
+
+abstract class ProductsService extends BaseProductsService {
+  Future getAllProduct();
+  
+}
+
+class ProductsServiceImp extends ProductsService {
+  @override
+  Future getAllProduct() async {
+    response = await dio.get(baseproducturl);
+    return response;
+  }
+  // @override
+  // Future SearchProuducts() async {
+  //   response = await dio.get('$baseproducturl+search?q=sear');
+  // }
+}
