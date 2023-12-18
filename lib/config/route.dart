6848073025f1cf2731a0335239a83c05222069ee
KaramFaraj <karam.faraj.ac@gmail.com>
2023-12-18 03:27:23 +0300

    base route and add splashscreen
    code the base of tree route and adding a stateful splash screen

---
+++ lib/config/route.dart
@@ -0,0 +1,10 @@
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => IntroPage(),
+    ),
+  ],
+);
